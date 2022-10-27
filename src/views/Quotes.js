4d3827cd4142ad6147dd35d67cdb4f67b3adde31
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2022-10-27 14:32:06 -0500

    basic setup done. Need to actually put the content in now

---
+++ src/views/Quotes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Quotes() {
+	return (
+		<Container fluid>
+			<Row>
+				<Col>
+                    <h1>Quotes Pages</h1>
+				</Col>
+			</Row>
+		</Container>
+	);
+}
+
+export default Quotes;
---
+++ src/views/characterCounter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function CharacterCounter() {
+	return (
+		<Container fluid>
+			<Row>
+				<Col>
+                    <h1>Character Counter Pages</h1>
+				</Col>
+			</Row>
+		</Container>
+	);
+}
+
+export default CharacterCounter;

2e8dcd6e80521cec90ffa438f3dc249d6041c8ec
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2022-10-27 16:23:40 -0500

    Got the env variable in

--- src/views/CTAapi.js
+++ src/views/CTAapi.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function CTAapi() {
+	console.log(process.env.REACT_APP_CTA_API)
+
+
 	return (
 		<Container fluid>
 			<Row>

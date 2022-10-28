1bb83ab7cff3fcc9eae9e9684c6dbd311bb0528e
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2022-10-28 12:09:12 -0500

    Didn't figure out CORS. May need to tie in express?

--- src/views/CTAapi.js
+++ src/views/CTAapi.js
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const baseURL =
-    'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=' +
+    'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=' +
     process.env.REACT_APP_CTA_API +
     '&mapid=40380&max=5';
 
 function CTAapi() {
     // console.log(process.env.REACT_APP_CTA_API)
-    const [res, setRes] = useState(null);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-        axios.get(baseURL).then(response => {
-            console.log(response);
-            setRes(response.data);
-        });
+        fetch(baseURL)
+            .then(res => res.json())
+            .then(d => {
+                setData(d);
+                console.log(data);
+            });
     }, []);
 
     return (
@@ -23,7 +25,7 @@ function CTAapi() {
             <Row>
                 <Col>
                     <h1>CTA API Pages</h1>
-                    <p>{res}</p>
+                    <p>{data}</p>
                 </Col>
             </Row>
         </Container>

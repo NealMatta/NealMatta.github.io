059752b48dd4d1d5f5ade8bdf536a4f37b9f09bf
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2022-10-28 18:58:30 -0500

    Almost figured out how to work with express. I'm following a tutorial. Curernt issue with that node-fetch doesn't work anymore. Need to use axios or something

--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const axios = require('axios');
 const app = express();
+const PORT = 3001;
 
 app.use(cors());
-dotenv.config({ path: '../.env' });
+const corsOptions = {
+    origin: 'http://localhost:3000',
+};
+const requestEndpoint =
+    'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=' +
+    process.env.REACT_APP_CTA_API +
+    '&mapid=40380&max=5';
 
-app.get('/CTAapi', (req, res) => {
-    const baseURL =
-        'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=' +
-        process.env.REACT_APP_CTA_API +
-        '&mapid=40380&max=5';
-
-    res.json({ users: ['u1', 'u2', 'u3'] });
+app.get('/getCTA', cors(corsOptions), async (req, res) => {
+    const fetchOptions = {
+        method: 'GET',
+    };
+    // const response = await fetch(requestEndpoint, fetchOptions);
+    const response = await axios(requestEndpoint);
+    console.log(response.data);
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
 });
 
 app.get('/api', (req, res) => {
     res.json({ users: ['u1', 'u2', 'u3'] });
 });
 
-app.listen(3001, () => {
-    console.log('Server started on port 3001');
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });

7538c67a46636a3b91f2ff23aec4b71220340b1d
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2022-11-01 18:51:44 -0500

    Testing out netlify branch deployment

---
+++ client/src/views/GoogleCalendarAddTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GoogleCalendarAddTask() {
+    return (
+        <div>
+            <h1>Google Calendar Add Task</h1>
+        </div>
+    );
+}
+
+export default GoogleCalendarAddTask;

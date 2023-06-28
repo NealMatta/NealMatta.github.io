b19b03efddb8309c4c57c4176fc287d42b2cd06f
Neal Matta <neal.matta@AMONGEQUALILL.workdayinternal.com>
2023-06-28 14:52:52 -0500

    Implemented some error handling when grabbing active widgets. I need to update all of this so I can test this in different branches

--- client/src/services/widgetsServices.js
+++ client/src/services/widgetsServices.js
@@ -142,20 +142,20 @@ export async function getActiveWidgets(whatToFetch) {
         const payload = await axios.get(whatToFetch);
         return payload.data;
     } catch (error) {
-        if (process.env.NODE_ENV === 'development') {
-            if (error.response) {
-                // The client was given an error response (5xx, 4xx)
-                console.error(error.response.data);
-                // console.error(err.response.status);
-                // console.error(err.response.headers);
-            } else if (error.request) {
-                // The client never received a response, and the request was never left
-                console.error(error.request);
-            } else {
-                // Anything else
-                console.error('Error', error.message);
-            }
+        // if (process.env.NODE_ENV === 'development') {
+        if (error.response) {
+            // The client was given an error response (5xx, 4xx)
+            console.error(error.response.data);
+            // console.error(err.response.status);
+            // console.error(err.response.headers);
+        } else if (error.request) {
+            // The client never received a response, and the request was never left
+            console.error(error.request);
+        } else {
+            // Anything else
+            console.error('Error', error.message);
         }
+        // }
     }
 }
 
